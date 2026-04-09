b8c875cf9fa6f44ed49913dbbf4a61f7d4c0d76f
simplyBarbe <andrea.b-1999@libero.it>
2026-04-09 10:32:09 +0200

    urls

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -22,6 +22,10 @@ namespace Server
 
                 var builder = WebApplication.CreateBuilder(args);
                 builder.ConfigureSerilog();
+                
+                // Configure Railway Port
+                var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+                builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
                 CultureConfiguration.SetDefault();
 
